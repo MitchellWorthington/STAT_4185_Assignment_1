e3841e31c3fb38a182edf2aa73b892ffaae17672
Mitchell Worthington <mitchellworthington64@gmail.com>
2023-01-27 13:12:54 -0500

    decrypted file 2

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,17 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+onehalf = encrypted_message[::2]
+twohalf = encrypted_message[1::2]
+twohalf = twohalf[::-1]
+
+joined_list = []
+
+for i in range(len(encrypted_message)):
+    if i % 2 == 0:
+        joined_list.append(onehalf[i//2])
+    if i % 2 == 1:
+        joined_list.append(twohalf[i//2])
+
+output = ''.join(joined_list)
+print(output)
